153f5563f02e2320f1fb5d155adbcb79c9ee4430
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-04-03 19:45:36 -0300

    implement decrypt method

--- app/src/main/java/com/example/rodri/mypass/util/EncryptDecrypt.java
+++ app/src/main/java/com/example/rodri/mypass/util/EncryptDecrypt.java
@@ -4,6 +4,9 @@ package com.example.rodri.mypass.util;
 import java.util.Random;
 
 /**
+ *
+ *  These are very very simple encrypt/decrypt methods which I implemented myself.
+ *
  * Created by rodri on 4/2/2016.
  */
 public class EncryptDecrypt {
@@ -14,6 +17,17 @@ public class EncryptDecrypt {
         return _key;
     }
 
+
+    /**
+     *
+     * 1 - backwards
+     * 2 - put the three first characters to the final of String
+     * 3 - move the second-to-last letter to the first position
+     * 4 - add a random number(0 to 9) in the first and last position;
+     *
+     * @param password
+     * @return
+     */
     public String encrypt(String password) {
 
         int n = password.length();
@@ -88,8 +102,79 @@ public class EncryptDecrypt {
 
     }
 
+    /**
+     *
+     * 1 - remove first and last characters
+     * 2 - move the first character to the second-to-last position
+     * 3 - move the last three characters to the beginning
+     * 4 - backwards
+     *
+     * @param encryptedPassword
+     * @return
+     */
     public String decrypt(String encryptedPassword) {
-        return "";
+
+        int n = encryptedPassword.length();
+        String[] tempArray = encryptedPassword.split("");
+        String[] decPass = new String[n - 2];
+
+
+
+        // STEP 1
+        int count = 0;
+        for (int i = 1; i < n - 1; i++) {
+            decPass[count++] = tempArray[i];
+        }
+        // END OF STEP 1
+
+
+        // STEP 2
+        String firstCharacter = decPass[0];
+        for (int i = 0; i < decPass.length - 2; i++) {
+            decPass[i] = decPass[i + 1];
+        }
+        decPass[decPass.length - 2] = firstCharacter;
+        // END OF STEP 2
+
+
+
+        // STEP 3
+        String[] lastThreeCharacters = new String[3];
+        count = 0;
+        for (int i = decPass.length - 1; i > 2; i--) {
+            if (i > decPass.length - 4) {
+                lastThreeCharacters[count++] = decPass[i];
+            }
+            decPass[i] = decPass[i - 3];
+        }
+        count = 2;
+        for (int i = 0; i < 3; i++) {
+            decPass[i] = lastThreeCharacters[count--];
+        }
+        // END OF STEP 3
+
+
+
+        // STEP 4
+        String[] forward = new String[n - 2];
+        count = 0;
+        for (int i = decPass.length - 1; i >= 0; i--) {
+            forward[count++] = decPass[i];
+        }
+        // END OF STEP 4
+
+
+
+        // convert from string array to string
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < forward.length; i++) {
+            strBuilder.append(forward[i]);
+        }
+        String result = strBuilder.toString();
+
+        return result;
+
+
     }
 
 }
