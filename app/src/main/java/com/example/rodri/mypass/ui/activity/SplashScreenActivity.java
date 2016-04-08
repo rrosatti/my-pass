e8a9e4c4d17836664941188d7a3cc1089a6b611e
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-04-08 10:15:02 -0300

    change logo and color theme

--- app/src/main/java/com/example/rodri/mypass/ui/activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/rodri/mypass/ui/activity/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.rodri.mypass.ui.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.widget.ImageView;
 
 import com.example.rodri.mypass.R;
@@ -20,8 +22,24 @@ public class SplashScreenActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+
+        /** Display will be used to get the width and height of the phone screen */
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        int width = size.x;
+        int height = size.y;
+
+        System.out.println("width: " + width + " height: " + height);
+
         ImageView img = (ImageView) findViewById(R.id.lockImageView);
-        img.setImageResource(R.drawable.lock);
+        img.setImageResource(R.drawable.key);
+        img.getLayoutParams().width = (int) (0.70 * width);
+        img.getLayoutParams().height = (int) (0.45 * height);
+        img.requestLayout();
+
+        System.out.println("width: " + (int) (0.6 * width) + " height: " + (int) (0.3 * height));
 
         final Thread splahThread = new Thread() {
             public void run() {
